ffb766ea045f935c1a247b28bc3f1f1fe35c09f0
deltafixer <gospodarweba@gmail.com>
2020-08-31 21:40:05 +0200

    fix(Profile panel): watch changes on welcome user message when new login

--- VehicleDiary/Main/ViewModels/ProfilePanelViewModel.cs
+++ VehicleDiary/Main/ViewModels/ProfilePanelViewModel.cs
@@ -1,17 +1,38 @@
-﻿using VehicleDiary.Models;
+﻿using Caliburn.Micro;
+using VehicleDiary.Main.Messages;
+using VehicleDiary.Models;
 using VehicleDiary.Services;
 
 namespace VehicleDiary.Main.ViewModels
 {
-    public class ProfilePanelViewModel
+    public class ProfilePanelViewModel : Screen, IHandle<DataMessage>
     {
+        private readonly IEventAggregator _eventAggregator;
         private readonly UserService _userService;
+        private string _welcomeUserMessage;
+        
         public UserModel User { get => _userService.User; }
-        public string WelcomeUserMessage => "Welcome, " + _userService.User?.Username;
 
-        public ProfilePanelViewModel(UserService userService)
+        public string WelcomeUserMessage { get => "Welcome, " + _welcomeUserMessage; set => Set(ref _welcomeUserMessage, value); }
+
+        public ProfilePanelViewModel(IEventAggregator eventAggregator, UserService userService)
         {
+            _eventAggregator = eventAggregator;
+            _eventAggregator.Subscribe(this);
             _userService = userService;
+        }
+
+        protected override void OnActivate()
+        {
+            base.OnActivate();
+        }
+
+        public void Handle(DataMessage message)
+        {
+            if (message.Message == DataMessages.USER)
+            {
+                WelcomeUserMessage = _userService.User.Username;
+            }
         }
     }
 }
